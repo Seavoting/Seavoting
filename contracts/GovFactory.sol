d81df0543eb7032e0ea284c07f9451302391049a
Seavoting <100253356+Seavoting@users.noreply.github.com>
2023-03-18 17:00:53 +0800

    Update GovFactory.sol

--- contracts/GovFactory.sol
+++ contracts/GovFactory.sol
@@ -1,19 +1,1 @@
-pragma experimental ABIEncoderV2;
 
-pragma solidity ^0.5.16;
-
-import './GovernorAlpha.sol';
-
-contract GovFactory {
-    PublicStruct.govInfo[] public govs;
-    function newGov(address timelock_, address comp_, address guardian_,string memory name_, string memory logo_) public returns(PublicStruct.govInfo memory) {
-        GovernorAlpha gov = new GovernorAlpha(timelock_,comp_,guardian_);
-        PublicStruct.govInfo memory govInfo = PublicStruct.govInfo({
-            govAddress:address(gov),
-            name:name_,
-            logo:logo_
-        });
-        govs.push(govInfo);
-        return govInfo;
-    }
-}
